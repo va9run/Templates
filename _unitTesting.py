b2a30d933647acc974852a1f17503cb9f889c181
varun yadav <varun@keyninja.io>
2022-07-29 11:40:46 +1000

    Unit Test to compare dataframe template

---
+++ _unitTesting.py
@@ -0,0 +1,15 @@
+# Compare DataFrame
+import pandas as pd
+import numpy as np
+import unittest
+from pandas.testing import assert_frame_equal
+
+
+class testmethod(unittest.TestCase):
+    def test_df_equals_to_df1(self,):
+        df2 = pd.read_parquet('UnitTestingDf1.parquet')
+        df1 = df2.copy()
+        return (assert_frame_equal(df2,df1))
+
+# In Terminal run python -m unittest #NAMEOFTHEMODULE(_unitTesting)
+# python -m unittest _unitTesting
